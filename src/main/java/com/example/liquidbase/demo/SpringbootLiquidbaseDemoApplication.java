4eaf655689dfe62ad1983bbab74c86fb5a18f49c
Neri Morales <neriunam@hotmail.com>
2020-06-04 16:54:57 -0500

    Initial commit files, project empty

---
+++ src/main/java/com/example/liquidbase/demo/SpringbootLiquidbaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquidbase.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLiquidbaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLiquidbaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/liquidbase/demo/SpringbootLiquidbaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liquidbase.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLiquidbaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
