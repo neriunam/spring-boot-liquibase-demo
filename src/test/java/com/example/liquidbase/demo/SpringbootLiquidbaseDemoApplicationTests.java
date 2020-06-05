7779623bd81c2c3fc7585751dddc29ce9a0fb957
Neri Morales <neriunam@hotmail.com>
2020-06-04 21:42:26 -0500

    Configuracion de liquidbase
    - Creacion de xml master change log (vacio)
    - Se agregaron las propiedades para el datasource y se habilito
    liquidbase
    - Se modifico test default para que no generara errores

--- src/test/java/com/example/liquidbase/demo/SpringbootLiquidbaseDemoApplicationTests.java
+++ src/test/java/com/example/liquidbase/demo/SpringbootLiquidbaseDemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.liquidbase.demo;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.liquidbase.demo.util.HsqlDBUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
+@Slf4j
 class SpringbootLiquidbaseDemoApplicationTests {
 
+	@BeforeAll
+	public static void initialize() {
+		log.info("Initialize test");
+		HsqlDBUtils.startHsqldbServer();
+	}
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() { }
+	
+	@AfterAll
+	public static void finalizeTest() {
+		HsqlDBUtils.stopHsqldbServer();
 	}
 
 }
