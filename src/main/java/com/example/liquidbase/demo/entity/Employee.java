98cedef3a8833698f8b76e9e04187487db746ce9
Neri Morales <neriunam@hotmail.com>
2020-06-05 14:12:02 -0500

    Se agrega composicion de employee con department

--- src/main/java/com/example/liquidbase/demo/entity/Employee.java
+++ src/main/java/com/example/liquidbase/demo/entity/Employee.java
@@ -2,6 +2,8 @@ package com.example.liquidbase.demo.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -17,5 +19,9 @@ public class Employee {
 	private String firstName;
 
 	private String lastName;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_department")
+	private Department department;
 	
 }
